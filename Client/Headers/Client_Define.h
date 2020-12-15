849cdce05825bbaf4ac1309fa440a692a2d084de
LAPTOP-60017TF8\ted00 <ted002@naver.com>
2020-12-15 12:12:02 +0900

    Start!

---
+++ Client/Headers/Client_Define.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef __CLIENT_DEFINE_H__
+#define __CLIENT_DEFINE_H__
+
+#define WINCX 1920
+#define WINCY 1080
+
+#endif // !__CLIENT_DEFINE_H__
---
+++ Client/Headers/MainApp.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __MAINAPP_H__
+#define __MAINAPP_H__
+
+class CMainApp final
+{
+public:
+	explicit CMainApp();
+	virtual ~CMainApp();
+
+};
+
+#endif // !__MAINAPP_H__
